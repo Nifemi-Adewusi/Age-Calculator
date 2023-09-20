8f1650a1f47de70023c2a0ae68602534abc62e8e
nadesTech <nifemiadewusi@gmail.com>
2023-09-20 09:17:11 +0300

    Add JavaScript to test almost all possible cases

--- app.js
+++ app.js
@@ -4,61 +4,75 @@ let userInput = document.getElementById("date");
 userInput.max = new Date().toISOString().split("T")[0];
 const result = document.querySelector(".result");
 const button = document.querySelector("button");
-// function calculateAge(input) {
-//   if (userInput.value === "") {
-//     return (result.innerHTML =
-//       "Please Select A Valid Age From The Date Picker.");
-//   }
-//   let dob = new Date(input);
-//   let today = new Date();
-//   // Age In Milliseconds
-//   const ageInMilliseconds = today.getTime() - dob.getTime();
-//   const years = Math.floor(ageInMilliseconds / (1000 * 60 * 60 * 24 * 365));
-//   const month = Math.floor(
-//     (ageInMilliseconds % (1000 * 60 * 60 * 24 * 365)) /
-//       (1000 * 60 * 60 * 24 * 30)
-//   );
-//   const day = Math.floor(
-//     (ageInMilliseconds % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24)
-//   );
-//   if (years === 0) {
-//     if (month === 0) {
-//       if (day === 0) {
-//         return `Welcome To The World`;
-//       } else if (day === 1) {
-//         return `You Are a day old`;
-//       } else {
-//         return `You Are ${day} days old`;
-//       }
-//     }
-//   }
-//   return `You are ${years} ${years === 1 ? "year" : "years"} ${month} ${
-//     month === 1 ? "month" : "months"
-//   } and ${day} ${day === 1 ? "day old" : "days old"}`;
-// }
 
 function checkInputValue() {
   if (userInput.value === "") {
     return (result.innerHTML =
       "Please select a valid date from the date Picker field");
   } else {
-    return userInput.value;
+    return calculateAge(userInput.value);
   }
 }
 
-function calculateAge(userInput) {
-  const dob = new Date(userInput);
+function checkDay(day) {
+  if (day === 1) {
+    return `a day old`;
+  } else if (day === 0) {
+    return `And it's your birthdate.`;
+  } else if (day > 1) {
+    return `${day} days old`;
+  }
+}
+
+function checkMonth(months) {
+  if (months === 0) {
+    return ``;
+  } else if (months === 1) {
+    return `,a month and`;
+  } else if (months > 1) {
+    return `,${months} months and`;
+  }
+}
+
+function placeHolder(day) {
+  if (day === 0) {
+    return ``;
+  } else {
+    return `You are`;
+  }
+}
+
+function checkYear(year) {
+  if (year === 1) {
+    return `A year`;
+  } else if (year === 0) {
+    return "";
+  } else if (year > 1) {
+    return `${year} years`;
+  }
+}
+
+function calculateAge(dobInput) {
+  const dob = new Date(dobInput);
   const today = new Date();
   //  Date In Milliseconds
   const ageInMilliseconds = today.getTime() - dob.getTime();
   const yearInMilliseconds = 1000 * 60 * 60 * 24 * 365;
-  const monthMilliseconds = yearInMilliseconds / (1000 * 60 * 60 * 24 * 30);
-  const dayMilliseconds = yearInMilliseconds / (1000 * 60 * 60 * 24 );
- const year = Math.floor(ageInMilliseconds / (yearInMilliseconds));
- const month = Math.floor(age)
+
+  const year = Math.floor(ageInMilliseconds / yearInMilliseconds);
+  const month = Math.floor(
+    (ageInMilliseconds % yearInMilliseconds) / (1000 * 60 * 60 * 24 * 30)
+  );
+  const day = Math.floor(
+    (ageInMilliseconds % yearInMilliseconds) / (1000 * 60 * 60 * 24)
+  );
+
+  return `${placeHolder(day)} ${checkYear(year)} ${checkMonth(
+    month
+  )} ${checkDay(day)}`;
 }
 
 button.addEventListener("click", function () {
-  const age = calculateAge(userInput.value);
+  const age = checkInputValue();
   result.innerHTML = age;
 });
